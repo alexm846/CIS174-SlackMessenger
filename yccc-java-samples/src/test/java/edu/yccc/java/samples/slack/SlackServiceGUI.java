dc82658e5ceb1ab2062817fe88e7f87a2fd74dc5
Alex McMahon <alexm_846@hotmail.com>
2018-04-12 07:07:59 -0400

    second commit

--- yccc-java-samples/src/test/java/edu/yccc/java/samples/slack/SlackServiceGUI.java
+++ yccc-java-samples/src/test/java/edu/yccc/java/samples/slack/SlackServiceGUI.java
@@ -1,30 +1,30 @@
 package edu.yccc.java.samples.slack;
+/**
+ * Slack Messenger GUI - Project 3 -- the meat is in the button Actions
+ * 
+ * 
+ * @author alexMcMahon
+ *
+ */
 
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-/**
- * Slack Messenger GUI 
- * 
- * 
- * @author aMcMahon
- *
- */
-
 public class SlackServiceGUI {
 
-	private JFrame frmSlackService;
+	private JFrame frmSlackMessenger;
 	private JTextField usernameField;
-	private JTextField channelField;
+	private JComboBox<String> channelBox;
 	private JTextArea messageArea;
 	private String username;
 	private String channel;
@@ -38,7 +38,7 @@ public class SlackServiceGUI {
 			public void run() {
 				try {
 					SlackServiceGUI window = new SlackServiceGUI();
-					window.frmSlackService.setVisible(true);
+					window.frmSlackMessenger.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -51,13 +51,10 @@ public class SlackServiceGUI {
 	 */
 	public SlackServiceGUI() {
 		initialize();
-		
-		
 	}
-	
 	public void getContent() {
 		username = usernameField.getText();
-		channel = channelField.getText();
+		channel = (String) channelBox.getSelectedItem();
 		message = messageArea.getText();
 	}
 
@@ -65,63 +62,74 @@ public class SlackServiceGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmSlackService = new JFrame();
-		frmSlackService.setTitle("Slack Service");
-		frmSlackService.setBounds(100, 100, 450, 483);
-		frmSlackService.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmSlackService.getContentPane().setLayout(null);
+		frmSlackMessenger = new JFrame();
+		frmSlackMessenger.setTitle("Slack Messenger");
+		frmSlackMessenger.setBounds(100, 100, 650, 503);
+		frmSlackMessenger.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSlackMessenger.getContentPane().setLayout(null);
 		
-		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblUsername.setBounds(10, 11, 89, 24);
-		frmSlackService.getContentPane().add(lblUsername);
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Californian FB", Font.BOLD, 18));
+		lblUsername.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsername.setBounds(70, 11, 202, 40);
+		frmSlackMessenger.getContentPane().add(lblUsername);
+		
+		JLabel lblChannel = new JLabel("Channel");
+		lblChannel.setFont(new Font("Californian FB", Font.BOLD, 18));
+		lblChannel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblChannel.setBounds(322, 11, 302, 40);
+		frmSlackMessenger.getContentPane().add(lblChannel);
 		
 		usernameField = new JTextField();
-		usernameField.setBounds(88, 13, 214, 24);
-		frmSlackService.getContentPane().add(usernameField);
+		usernameField.setBounds(46, 48, 266, 33);
+		frmSlackMessenger.getContentPane().add(usernameField);
 		usernameField.setColumns(10);
 		
+		channelBox = new JComboBox<String>();
+		channelBox.setBounds(348, 48, 248, 33);
+		frmSlackMessenger.getContentPane().add(channelBox);
+		channelBox.setEditable(true);
+		channelBox.addItem("#computersaretalking");
+		channelBox.addItem("#general");
+		channelBox.addItem("#integration");
+		channelBox.addItem("enter Channel Name");
 		
-		JLabel lblChannel = new JLabel("Channel:");
-		lblChannel.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblChannel.setBounds(10, 57, 89, 24);
-		frmSlackService.getContentPane().add(lblChannel);
-		
-		channelField = new JTextField();
-		channelField.setBounds(88, 59, 214, 24);
-		frmSlackService.getContentPane().add(channelField);
-		channelField.setColumns(10);
-		
-		
-		JLabel lblMessasge = new JLabel("Message:");
-		lblMessasge.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblMessasge.setBounds(10, 107, 81, 24);
-		frmSlackService.getContentPane().add(lblMessasge);
+		JLabel lblMessage = new JLabel("Message");
+		lblMessage.setFont(new Font("Californian FB", Font.BOLD, 18));
+		lblMessage.setBounds(46, 105, 266, 40);
+		frmSlackMessenger.getContentPane().add(lblMessage);
 		
 		messageArea = new JTextArea();
-		messageArea.setLineWrap(true);
-		messageArea.setBounds(10, 142, 414, 231);
-		frmSlackService.getContentPane().add(messageArea);
-		
-		
-		JButton btnSend = new JButton("Send");
-		btnSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
+		messageArea.setBounds(46, 144, 550, 241);
+		frmSlackMessenger.getContentPane().add(messageArea);
+		//JButton: Inheritance & interface! extends AbstractButton & implements accessible
+		JButton btnSend = new JButton("Send"); 
+		btnSend.addActionListener(new ActionListener() { //What? ActionListener is an interface!
+			public void actionPerformed(ActionEvent arg0) {//interface method
 				getContent(); //get input text
-				frmSlackService.setVisible(false);
-				System.out.println(username + " " + channel + " " + message);
+				frmSlackMessenger.setVisible(false);
+				System.out.println("Success: Username: " + username + "  Channel: " +channel + "  Message: " 
+				+ message + ".\n");
+				printQuestions();
 				SlackMessengerApp go = new SlackMessengerApp();
 				go.testSendMessage(username, channel, message); //send message thru SlackService
 			}
 		});
-		btnSend.setFont(new Font("Tahoma", Font.BOLD, 14));
-		btnSend.setBounds(320, 383, 89, 50);
-		frmSlackService.getContentPane().add(btnSend);
+		btnSend.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnSend.setBounds(473, 396, 120, 57);
+		frmSlackMessenger.getContentPane().add(btnSend);
 	}
-	
-	
-
-
-
+	public void printQuestions() {
+		System.out.println("What is Mavan?");
+		System.out.println("Maven is a build automation tool, with the following objectives:\n"
+				+ "Making the build process easy\r\n" + 
+				"Providing a uniform build system\r\n" + 
+				"Providing quality project information\r\n" + 
+				"Providing guidelines for best practices development\r\n" + 
+				"Allowing transparent migration to new features\n");
+		System.out.println("What is a jar file?");
+		System.out.println("JAR stands for Java Archive.  A JAR file allows Java runtimes to \"efficiently \n"
+				+ "deploy an entire application, including its classes and their associated resources, in \n"
+				+ "a single request,\" where elements may be compressed to save time.");
+	}
 }
